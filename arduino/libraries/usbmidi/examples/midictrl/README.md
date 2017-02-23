b57a8c5cae469ee530c9974e47cce9edaba1ce1e
Giedrius <giedrius@blokas.io>
2017-02-24 01:02:15 +0200

    Adding midictrl example with 4 potentiometers and 2 push buttons.

---
+++ arduino/libraries/usbmidi/examples/midictrl/README.md
@@ -0,0 +1,18 @@
+# midictrl Example
+
+![midictrl Wiring Diagram](https://github.com/BlokasLabs/usbmidi/blob/master/arduino/libraries/usbmidi/examples/midictrl/midictrl.png)
+
+# Required Parts
+
+* 4x 10k potentiometers.
+* 2x push buttons.
+* 1x Arduino compatible board based on ATmega32U4 or other microcontroller compatible with Pluggable USB library. We used SparkFun Pro Micro (https://www.sparkfun.com/products/12640)
+* 1x breadboard, or soldering iron and some wires.
+
+# Steps
+
+First, install usbmidi library to your Arduino IDE, see [here](https://github.com/BlokasLabs/usbmidi) for instructions.
+
+Then just connect all the parts as shown in the wiring diagram. If using a different Arduino board, make sure that potentiometers are connected to ADC pins and push buttons are connected to any digital pins, and you will have to adjust the analogPins and buttonPins in the sketch according to the connections you have actually made.
+
+After flashing the sketch into the Arduino, it will be sending MIDI CC messages on controls 0-3 and Note On/Off messages for notes 64 and 65.
---
+++ arduino/libraries/usbmidi/examples/midictrl/midictrl.ino
@@ -0,0 +1,80 @@
+#include <midi_serialization.h>
+#include <usbmidi.h>
+
+// See midictrl.png in the example folder for the wiring diagram,
+// as well as README.md.
+
+void sendCC(uint8_t channel, uint8_t control, uint8_t value) {
+	USBMIDI.write(0xB0 | (channel & 0xf));
+	USBMIDI.write(control & 0x7f);
+	USBMIDI.write(value & 0x7f);
+}
+
+void sendNote(uint8_t channel, uint8_t note, uint8_t velocity) {
+	USBMIDI.write((velocity != 0 ? 0x90 : 0x80) | (channel & 0xf));
+	USBMIDI.write(note & 0x7f);
+	USBMIDI.write(velocity &0x7f);
+}
+
+const int ANALOG_PIN_COUNT = 4;
+const int BUTTON_PIN_COUNT = 2;
+
+// Change the order of the pins to change the ctrl or note order.
+int analogPins[ANALOG_PIN_COUNT] = { A3, A2, A1, A0 };
+int buttonPins[BUTTON_PIN_COUNT] = { 9, 8 };
+
+int ccValues[ANALOG_PIN_COUNT];
+int buttonDown[BUTTON_PIN_COUNT];
+
+int readCC(int pin) {
+	// Convert from 10bit value to 7bit.
+	return analogRead(pin) >> 3;
+}
+
+int isButtonDown(int pin) {
+	return digitalRead(pin) == 0;
+}
+
+void setup() {
+	// Initialize initial values.
+	for (int i=0; i<ANALOG_PIN_COUNT; ++i) {
+		pinMode(analogPins[i], INPUT);
+		ccValues[i] = readCC(analogPins[i]);
+	}
+
+	for (int i=0; i<BUTTON_PIN_COUNT; ++i) {
+		pinMode(buttonPins[i], INPUT);
+		digitalWrite(buttonPins[i], HIGH);
+		buttonDown[i] = isButtonDown(buttonPins[i]);
+	}
+}
+
+void loop() {
+	while (USBMIDI.available()) {
+		// We must read entire available data, so in case we receive incoming
+		// MIDI data, the host wouldn't get stuck.
+		u8 b = USBMIDI.read();
+	}
+
+	for (int i=0; i<ANALOG_PIN_COUNT; ++i) {
+		int value = readCC(analogPins[i]);
+
+		// Send CC only if th has changed.
+		if (ccValues[i] != value) {
+			sendCC(0, i, value);
+			ccValues[i] = value;
+		}
+	}
+
+	for (int i=0; i<BUTTON_PIN_COUNT; ++i) {
+		int down = isButtonDown(buttonPins[i]);
+
+		if (down != buttonDown[i]) {
+			sendNote(0, 64 + i, down ? 127 : 0);
+			buttonDown[i] = down;
+		}
+	}
+
+	// Flush the output.
+	USBMIDI.flush();
+}
