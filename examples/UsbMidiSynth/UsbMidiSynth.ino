8cbd2a7ca037f744810b4399b00a92dfaa1858d1
Tomas Mudrunka <tomas@mudrunka.cz>
2019-01-07 15:17:21 +0100

    Fixed some midi parsing issues

--- examples/UsbMidiSynth/UsbMidiSynth.ino
+++ examples/UsbMidiSynth/UsbMidiSynth.ino
@@ -1,5 +1,5 @@
 /*
- * Simple USBMIDI synth v0.1
+ * Simple USBMIDI synth v0.2
  *
  * Plays MIDI notes using square waves on SNDOUT pin
  * Attach piezo or audio amplifier to that pin and send some notes to it
@@ -11,12 +11,12 @@
 
 #include <usbmidi.h>
 
-#define SNDOUT 1 //Audio output pin
+#define SNDOUT 10 //Audio output pin
 
-#define MIDI_NOTE_OFF 128
-#define MIDI_NOTE_ON  144
-#define MIDI_CONTROL  176
-#define MIDI_PITCH_BEND 224
+#define MIDI_NOTE_OFF   0b10000000
+#define MIDI_NOTE_ON    0b10010000
+#define MIDI_CONTROL    0b10110000
+#define MIDI_PITCH_BEND 0b11100000
 
 //Convert MIDI note to frequency
 double base_a4=440; //set A4=440Hz
@@ -37,23 +37,49 @@ void loop() {
   while (USBMIDI.available()) {
 
     //Parse MIDI
-    u8 command=0, channel=0, pitch=0;
+    u8 command=0, channel=0, key=0, pitchbend=0, pblo=0, pbhi=0, velocity=0;
+    while(!(USBMIDI.peek() & 0b10000000)) USBMIDI.read(); //Drop bad commands
     command = USBMIDI.read();
-    pitch   = USBMIDI.read();
-    channel = USBMIDI.read();
+    channel = (command & 0b00001111)+1;
+    command = command & 0b11110000;
+
+    switch(command) {
+      case MIDI_NOTE_ON:
+      case MIDI_NOTE_OFF:
+        if(USBMIDI.peek() & 0b10000000) continue; key      = USBMIDI.read();
+        if(USBMIDI.peek() & 0b10000000) continue; velocity = USBMIDI.read();
+        break;
+      case MIDI_PITCH_BEND:
+        if(USBMIDI.peek() & 0b10000000) continue; pblo = USBMIDI.read();
+        if(USBMIDI.peek() & 0b10000000) continue; pbhi = USBMIDI.read();
+        int pitchbend = (pblo << 7) | pbhi;
+        //TODO: apply pitchbend to tone
+        break;
+    }
 
     //Play tones
-    if(command == MIDI_NOTE_ON && pitch > 0) tone(SNDOUT, note_to_freq(pitch));
-    if(command == MIDI_NOTE_OFF || pitch == 0) noTone(SNDOUT);
+    unsigned int pitch = note_to_freq(key);
+    if(command == MIDI_NOTE_ON && velocity > 0 && pitch > 0) tone(SNDOUT, pitch);
+    if(command == MIDI_NOTE_OFF || velocity == 0 || pitch == 0) noTone(SNDOUT);
 
     //Debug
     Serial.print(command);
-    Serial.print(' ');
-    Serial.print(pitch);
-    Serial.print(' ');
+    Serial.print(" C:");
     Serial.print(channel);
-    Serial.print(' ');
-    Serial.print(note_to_freq(pitch));
+    Serial.print(" K:");
+    Serial.print(key);
+    Serial.print("\tV:");
+    Serial.print(velocity);
+    Serial.print("\tP:");
+    Serial.print(pitch);
+    Serial.print("\tPB:");
+    Serial.print(pitchbend, BIN);
+    Serial.print(" (");
+    Serial.print(pblo, BIN);
+    Serial.print(" ");
+    Serial.print(pbhi, BIN);
+    Serial.print(")\tF:");
+    Serial.print(pitch);
     Serial.print('\n');
   }
 }
